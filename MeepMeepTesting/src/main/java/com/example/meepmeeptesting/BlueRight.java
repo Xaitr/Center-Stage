bd9cfa47e93ba2b948acc8a214fa6a60dcae58b4
Flower405 <sabyllamitchell@gmail.com>
2023-10-26 20:01:54 -0600

    Added a servo to start spining when intake starts

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/BlueRight.java
@@ -0,0 +1,35 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class BlueRight {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-35, 57, Math.toRadians(-270)))
+                        .lineTo(new Vector2d(-53,52))
+                        .lineTo(new Vector2d(-53, 34))
+                        .splineTo(new Vector2d(-53,10), Math.toRadians(0))
+                        .splineToConstantHeading(new Vector2d (-16,10), Math.toRadians(0))
+                        .splineToConstantHeading(new Vector2d (38,10), Math.toRadians(0))
+                        .splineToConstantHeading(new Vector2d(50,34), Math.toRadians(0))
+                        .turn(Math.toRadians(0))
+                        .splineToConstantHeading(new Vector2d (58,14), Math.toRadians(0))
+                        .build()
+
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
